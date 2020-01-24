f038be800c4cabe042d22a30fef9f644e66ece4d
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-24 20:20:30 +0000

    room class passes all getter tests

--- room.rb
+++ room.rb
@@ -1,3 +1,17 @@
 class Room
 
+  attr_reader :name, :capacity, :price, :songs
+
+  def initialize(name, capacity, price, songs)
+    @name = name
+    @capacity = capacity
+    @price = price
+    @songs = songs
+    @occupents = []
+  end
+
+  def get_occupents
+    return @occupents
+  end
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,24 +3,51 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../room')
+require_relative('../guest')
+require_relative('../song')
 
 class TestRoom < Minitest::Test
 
   def setup
 
-    @bohemian_rhapsody = Song.new("Bohemian Rhapsody", "Queen", 0)
-    @wonderwall = Song.new("Wanderwall", "Oasis", 0)
-    @africa = Song.new("Africa", "Toto" 0)
-    @dancing_queen = Song.new("Dancing Queen", "Abba", 0)
-    @the_jackal = Song.new("The Jackal", "Unknown", 0)
+    @bohemian_rhapsody = Song.new("Bohemian Rhapsody", "Queen")
+    @wonderwall = Song.new("Wanderwall", "Oasis")
+    @africa = Song.new("Africa", "Toto")
+    @dancing_queen = Song.new("Dancing Queen", "Abba")
+    @the_jackal = Song.new("The Jackal", "Unknown")
     @songs = [@bohemian_rhapsody, @wonderwall, @africa, @dancing_queen, @the_jackal]
 
-    @josh = Guest.new("Josh", 33, 50, @wonderwall)
-    @toby = Guest.new("Toby", 40, 55, @bohemian_rhapsody)
-    @cj = Guest.new ("CJ", 35, 60, @the_jackal)
-    @zoey = Guest.new ("Zoey", 17, 30, @dancing_queen)
+    @josh = Guest.new("Josh", 33, 50, "wonderwall")
+    @toby = Guest.new("Toby", 40, 55, "bohemian_rhapsody")
+    @cj = Guest.new("CJ", 35, 60, "the_jackal")
+    @zoey = Guest.new("Zoey", 17, 30, "dancing_queen")
     @guests = [@josh, @toby, @cj, @zoey]
 
+    @room101 = Room.new("Room 101", 4, 5, @songs)
+
   end
+
+  def test_get_name
+    assert_equal("Room 101", @room101.name)
+  end
+
+  def test_get_capacity
+    assert_equal(4, @room101.capacity)
+  end
+
+  def test_get_price
+    assert_equal(5, @room101.price)
+  end
+
+  def test_get_songs
+    assert_equal([@bohemian_rhapsody, @wonderwall, @africa, @dancing_queen, @the_jackal], @room101.songs)
+
+  end
+
+  def test_get_occupents
+    assert_equal([], @room101.get_occupents)
+  end
+
+
 
 end
