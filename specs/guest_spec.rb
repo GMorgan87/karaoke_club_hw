71e1440a38ddf36c26ca2bf036bc0753ca1acf43
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-24 19:51:41 +0000

    adds tests to guest_spec

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -8,6 +8,33 @@ class TestGuest < Minitest::Test
 
   def setup
 
+    @josh = Guest.new("Josh", 33, 50, "Wonderwal")
+    @toby = Guest.new("Toby", 40, 55, "Bohemian Rhapsody")
+    @cj = Guest.new ("CJ", 35, 60, "The Jackal")
+
+  end
+
+  def test_get_name
+    assert_equal("Josh", @josh.name)
+  end
+
+  def test_get_age
+    assert_equal(30, @toby.age)
+  end
+
+  def test_get_cash
+    assert_equal(60, @cj.cash)
+  end
+
+  def test_get_favourite_song
+    assert_equal(@the_jackal, @cj.favourite_song)
+  end
+
+  def test_decrease_cash
+    @josh.pay(20)
+    assert_equal(30, @josh.cash)
+  end
+
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,13 +9,17 @@ class TestRoom < Minitest::Test
   def setup
 
     @bohemian_rhapsody = Song.new("Bohemian Rhapsody", "Queen", 0)
-    @wannabe = Song.new("Wannabe", "Spice Girls", 0)
-    @wonderwall = Song.new ("Wanderwall", "Oasis", 0)
-    @angels = Song.new ("Angels", "Robbie Williams", 0)
-    @africa = Song.new ("Africa", "Toto" 0)
-    @dancing_queen = Song.new ("Dancing Queen", "Abba", 0)
-    @country_house = SOng.new ("Country House", "Blur", 0)
-    @songs = [@bohemian_rhapsody, @wannabe, @wonderwall, @angels, @africa, @dancing_queen, @country_house]
+    @wonderwall = Song.new("Wanderwall", "Oasis", 0)
+    @africa = Song.new("Africa", "Toto" 0)
+    @dancing_queen = Song.new("Dancing Queen", "Abba", 0)
+    @the_jackal = Song.new("The Jackal", "Unknown", 0)
+    @songs = [@bohemian_rhapsody, @wonderwall, @africa, @dancing_queen, @the_jackal]
+
+    @josh = Guest.new("Josh", 33, 50, @wonderwall)
+    @toby = Guest.new("Toby", 40, 55, @bohemian_rhapsody)
+    @cj = Guest.new ("CJ", 35, 60, @the_jackal)
+    @zoey = Guest.new ("Zoey", 17, 30, @dancing_queen)
+    @guests = [@josh, @toby, @cj, @zoey]
 
   end
 
